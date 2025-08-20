5501182d2d727687050892fd0094380446f18e30
meistergpc <jap1centeno@gmail.com>
2025-08-20 22:40:12 +0200

    Enhance error handling middleware to include NOT_FOUND status and improve error messages for Prisma specific errors

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,4 +1,4 @@
-import { CONFLICT } from "../constants/http";
+import { CONFLICT, NOT_FOUND } from "../constants/http";
 
 export const errorHandler = (err, req, res, next) => {
   console.error("Error", err);
@@ -6,5 +6,18 @@ export const errorHandler = (err, req, res, next) => {
 
 // Prisma specific error
 if (err.code === "P2002") {
-  return res.status(CONFLICT).json({ error: "User already exists" });
+  return res.status(CONFLICT).json({ error: "unique constraint failed" });
 }
+
+if (err.code === "P2025") {
+  return res.status(NOT_FOUND).json({ error: "Record not found" });
+}
+
+if (err.code === "P2003") {
+  return res.status(CONFLICT).json({ error: "Foreign key constraint failed" });
+}
+
+// Default
+res.status(err.status || 500).json({
+  error: err.message || "Internal server error",
+});
