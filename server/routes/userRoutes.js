9a03aca76b03f807a080bd94425e69f184a3d6ed
meistergpc <jap1centeno@gmail.com>
2025-08-20 23:23:50 +0200

    Refactor controllers and services to improve error handling and streamline code; add asyncHandler middleware for better async error management

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { userController } from "../controllers/userController.js";
+import { asyncHandler } from "../middleware/asyncHandler.js";
 
 const router = express.Router();
 
-router.get("/", userController.getAll);
-router.get("/:id", userController.getById);
-router.post("/", userController.create);
-router.put("/:id", userController.update);
-router.delete("/:id", userController.delete);
+router.get("/", asyncHandler(userController.getAll));
+router.get("/:id", asyncHandler(userController.getById));
+router.post("/", asyncHandler(userController.create));
+router.put("/:id", asyncHandler(userController.update));
+router.delete("/:id", asyncHandler(userController.delete));
 
 export default router;
