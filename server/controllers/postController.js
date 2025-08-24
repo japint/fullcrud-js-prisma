5300c0a9a75e7238aa5bc6caba16fd520e0a1eb2
meistergpc <jap1centeno@gmail.com>
2025-08-24 23:34:31 +0200

    Refactor postController to use asyncHandler for all route handlers, improving error handling and consistency

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,13 +1,14 @@
+import { CREATED, NOT_FOUND } from "../constants/http.js";
 import { postServices } from "../services/postServices.js";
-import { BAD_REQUEST, CREATED, NOT_FOUND } from "../constants/http.js";
+import { asyncHandler } from "../middleware/asyncHandler.js";
 
 export const postController = {
-  getAll: async (req, res) => {
+  getAll: asyncHandler(async (req, res) => {
     const posts = await postServices.getAll();
     res.json(posts);
-  },
+  }),
 
-  getById: async (req, res) => {
+  getById: asyncHandler(async (req, res) => {
     const post = await postServices.getById(req.params.id);
     if (!post) {
       const err = new Error("Post not found");
@@ -15,26 +16,20 @@ export const postController = {
       throw err;
     }
     res.json(post);
-  },
+  }),
 
-  create: async (req, res) => {
-    const { title, authorId } = req.body;
-    if (!title || !authorId) {
-      const err = new Error("Title and Author ID are required");
-      err.status = BAD_REQUEST;
-      throw err;
-    }
+  create: asyncHandler(async (req, res) => {
     const post = await postServices.create(req.body);
     res.status(CREATED).json(post);
-  },
+  }),
 
-  update: async (req, res) => {
+  update: asyncHandler(async (req, res) => {
     const post = await postServices.update(req.params.id, req.body);
     res.json(post);
-  },
+  }),
 
-  delete: async (req, res) => {
+  delete: asyncHandler(async (req, res) => {
     await postServices.delete(req.params.id);
     res.json({ message: "Post deleted successfully" });
-  },
+  }),
 };
